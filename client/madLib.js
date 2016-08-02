99efa8003187f382916296bd33c8f907aa09f9e9
Sean Mooney <smooney914@gmail.com>
2016-08-01 20:45:20 -0400

    Final commit

--- client/madLib.js
+++ client/madLib.js
@@ -8,14 +8,15 @@ Template.body.events({
 
 	'submit #madLib-form': function(event, template) {
 		event.preventDefault();
-		console.log("Hello, world");
 
 		let exclamation = template.find('#exclamation').value;
 		let adverb = template.find('#adverb').value;
 		let noun = template.find('#noun').value;
 		let verb = template.find('#verb').value;
 		let madLib = "'" + exclamation + "!' you say, as you " + adverb + " jump onto your " + noun + " and " + verb + " into the sunset.";
-		
-		console.log(madLib);
+		$("#madLib").hide();
+		$("#madLib").text(madLib);
+		$("#madLib").css("color","#0000ff");
+		$("#madLib").fadeIn("slow");
 	}
 });
